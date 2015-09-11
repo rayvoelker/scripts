5ed576a8784b28052df3465094addffd3715c22a
rayvoelker <ray.voelker@gmail.com>
2015-09-11 08:41:20 -0400

    added instructions to the script

--- UD Safe Connect Log Off/logoff.js
+++ UD Safe Connect Log Off/logoff.js
@@ -7,7 +7,29 @@ ray.voelker@gmail.com
 adapted from:
 https://github.com/ariya/phantomjs/blob/master/examples/waitfor.js
 
-notes for future enhancement:
+
+
+notes and future enhancements:
+
+The intended use of this script is in conjunction with the software PhantomJS
+http://phantomjs.org/
+
+To log users off of the safe connect network automatically on workstation log off 
+do the following:
+1) Download and unpack the PhantomJS executable onto the workstation and place in
+a path easily accessible.
+
+2) Place this script on the workstation (putting it in the same path as the 
+PhantomJS executable is recommended)
+
+3) Modify the Startup/Shutdown, Logon/Logoff scripts using window group policy 
+editor ... adding a batch script similar to the following:
+	@echo off
+	start C:\phantomjs\phantomjs.exe C:\phantomjs\logoff.js
+	exit
+
+Issues
+------
 1) This script doesn't wait for the network, or continue to try the page x number
 of times if the network in unavailable. It's probably a good idea to add such 
 a thing to a future version of this script.
